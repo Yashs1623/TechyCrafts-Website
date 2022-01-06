2c571229f3ef58c5ba9c6d8ddf6ed31a8a2e5760
Yashs1623 <yashs230602@gmail.com>
2022-01-06 13:25:54 +0530

    Reponsive UI has been created and website build completed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#55BDCA',
+        'custom-light-blue':'#A8F2F2',
+        'custom-lightest-blue':'#E4F0FD'
+      },
+    },
+  },
+  plugins: [],
+}
